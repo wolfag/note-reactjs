b94babd37acfe6ed814812ac94cdbe8cc181b861
Admin <admin@Admins-MacBook-Pro.local>
2020-04-05 22:35:00 +0700

    config redux

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,15 +1,21 @@
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
+import { Provider } from 'react-redux';
 import theme from '../../common/Theme';
+import configureStore from '../../redux/configureStore';
 import Taskboard from '../Taskboard';
 import styles from './style';
 
+const store = configureStore();
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Taskboard />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Taskboard />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
